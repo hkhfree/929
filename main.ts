327eb26ab46aa2c2e2cd64546878b6a4a78e44d6
hkhfree <90696640+hkhfree@users.noreply.github.com>
2021-09-30 11:09:35 +0900

    경비시스템

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+input.onButtonPressed(Button.A, function () {
+    경비 = 1
+})
+input.onButtonPressed(Button.B, function () {
+    경비 = 0
+})
+let 경비 = 0
+radio.setGroup(6)
 basic.forever(function () {
-	
+    if (1 == pins.digitalReadPin(DigitalPin.P1)) {
+        radio.sendNumber(1)
+        basic.showIcon(IconNames.Heart)
+    } else {
+        radio.sendNumber(0)
+        basic.showIcon(IconNames.Yes)
+    }
 })
